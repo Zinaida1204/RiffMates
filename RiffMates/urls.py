fd64e8836f82ef4e1fa1363fde05c9c7e7af8947
Zinaida1204 <zinakrivosch3@gmail.com>
2025-05-17 16:46:33 +0700

    musician page

--- RiffMates/urls.py
+++ RiffMates/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from home import views as home_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("credits/", home_views.credits, name="credits")
+    path("credits/", home_views.credits, name="credits"),
+    path("bands/", include("bands.urls")),
 ]
